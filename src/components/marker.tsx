ea6097e7115cf35f944c3850b126db6929a8c9af
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-02-18 12:37:09 +0900

    情報ウィンドウからgoogle mapへ飛べるように変更

--- src/components/marker.tsx
+++ src/components/marker.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import ReactDOMServer from "react-dom/server";
 
 // MarkerProps 型に map を含める
-interface MarkerProps extends google.maps.MarkerOptions {}
+interface MarkerProps extends google.maps.MarkerOptions {
+  userPosition: google.maps.LatLngLiteral;
+}
 
 const Marker: React.FC<MarkerProps> = (options) => {
   // console.log("icon");
@@ -12,6 +15,26 @@ const Marker: React.FC<MarkerProps> = (options) => {
   // console.log(options.name);
   const [marker, setMarker] = useState<google.maps.Marker>();
   const [infoWindow, setInfoWindow] = useState<google.maps.InfoWindow>();
+  // console.log(options.position?.lat);
+  const windowContent = (
+    <div>
+      {options.title}
+      <br></br>
+      <a
+        href={`https://www.google.com/maps/dir/?api=1&origin=${String(
+          options.userPosition.lat
+        )}%2C${String(options.userPosition.lng)}&destination=${String(
+          options.position?.lat
+        )}%2C${String(options.position?.lng)}`}
+        target="_blank"
+      >
+        Goole Map で見る
+      </a>
+    </div>
+  );
+
+  const windowContentString = ReactDOMServer.renderToString(windowContent);
+  console.log(windowContentString);
   useEffect(() => {
     if (!marker) {
       setMarker(new google.maps.Marker());
@@ -30,14 +53,14 @@ const Marker: React.FC<MarkerProps> = (options) => {
     if (marker) {
       marker.setOptions(options);
       // marker.setIcon(icon)
-      infoWindow?.setContent(options.title);
+      infoWindow?.setContent(windowContentString);
       marker.addListener("click", () => {
         infoWindow?.open({
           anchor: marker,
         });
       });
     }
-  }, [marker, options]);
+  }, [marker, options, infoWindow, windowContentString]);
 
   return null;
 };
